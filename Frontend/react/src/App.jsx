4e97336fd1c4fe678e84be74b2f3b02c15d44a4b
YashasBNB <yashasnaidu3@gmail.com>
2024-05-03 18:53:47 +0530

    done

--- Frontend/react/src/App.jsx
+++ Frontend/react/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './Components/landingPage.jsx';
+import  Login from './Components/Login.jsx';
+import Signup from './Components/Signup.jsx';
+
 function App() {
   return (
     <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/Signup" element={<Signup />} />
+
     </Routes>
   );
 }
