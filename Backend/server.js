a8322b8114fbdb050db795ecad19977442b45755
YashasBNB <yashasnaidu3@gmail.com>
2024-06-04 19:29:17 +0530

    done

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PUBLIC_PORT || 3000;
-const dotenv = require('dotenv');
-dotenv.config();
 
-// define the /env route with the response in JSON
-
-app.get('/', (req,res)=> {
-  res.json({message:"pong"});
-})
-
+// define the ping route with the response in JSON
+app.get('/ping', (req, res) => {
+res.json({message: 'pong' });
+});
 
 if (require.main === module) {
   app.listen(port, () => {
     console.log(`🚀 server running on PORT: ${port}`);
   });
 }
+
 module.exports = app;
