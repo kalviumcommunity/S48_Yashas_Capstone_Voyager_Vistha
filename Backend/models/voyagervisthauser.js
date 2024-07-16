428184e0118d9a092989de4c25fb564ad4952c30
YashasBNB <yashasnaidu3@gmail.com>
2024-07-16 11:14:19 +0530

    done

---
+++ Backend/models/voyagervisthauser.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const UserSchema = new mongoose.Schema({  
+    name: String,
+    email: String,
+    age: Number,
+    password: String,
+    places: [{ name: String, experiences: [String], feedback: [String] }], 
+});
+
+
+
+const UserModal = mongoose.model("users", UserSchema);
+module.exports = {
+    UserModal
+};
